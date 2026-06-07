d5582a2084168cfca553433c31b21ef67fba6c71
yaponec432-hashu <234154007+yaponec432-hashu@users.noreply.github.com>
2026-06-07 13:13:25 +0300

    Update run.sh

--- .github/run.sh
+++ .github/run.sh
@@ -4,19 +4,20 @@
 set -e
 
 get_master_id() {
-  local master_id_file='./master_id'
-  until [[ -r "${master_id_file}" ]]; do
-    sleep 1
-  done
-  cat "${master_id_file}"
+    local master_id_file='./master_id'
+    until [[ -r "${master_id_file}" ]]; do
+      sleep 1
+    done
+    cat "${master_id_file}"
 }
 
 main() {
-  uv run ./master.py &
-  local MASTER_ID
-  MASTER_ID="$(get_master_id)"
-  MASTER_ID="${MASTER_ID}" uv run ./slave.py
-  # TODO: More workers, export
+    uv run ./master.py &
+    local MASTER_ID
+    MASTER_ID="$(get_master_id)"
+    date
+    MASTER_ID="${MASTER_ID}" uv run ./slave.py
+    # TODO: More workers, export
 }
 
 main
