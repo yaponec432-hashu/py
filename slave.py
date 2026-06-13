f857ce732c33af7183fa305d3ba828b030ddecfd
yaponec432-hashu <234154007+yaponec432-hashu@users.noreply.github.com>
2026-06-13 17:48:28 +0300

    Update slave.py

--- slave.py
+++ slave.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: 0BSD
 """A discord bot."""
 
-from logging import basicConfig, ERROR
+from logging import basicConfig, INFO
 from asyncio import wait_for, Runner
 from os import environ
 
@@ -89,6 +89,6 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    basicConfig(level=ERROR)
+    basicConfig(level=INFO)
     with Runner(loop_factory=new_event_loop) as runner:
         runner.run(main())
